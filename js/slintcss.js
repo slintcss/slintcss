3641f236a48eee8783fd18f6c8f561f13316f8df
Cavdy <cavdyofficials@gmail.com>
2019-02-21 12:47:57 +0100

    created bottom navigation system

--- js/slintcss.js
+++ js/slintcss.js
@@ -1,17 +1,22 @@
-const appbarBurger = document.querySelector('.appbar-burger');
-const appbarList = document.querySelector('.appbar-list');
-const appbarItem = document.querySelectorAll('.appbar-item');
-
-appbarBurger.addEventListener('click', () => {
-  appbarList.classList.toggle('appbar-list-active');
-
-  appbarItem.forEach((item, index) => {
-    if (item.style.animation) {
-      item.style.animation = '';
-    } else {
-      item.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-    }
+$(document).ready(function () {
+  // Appbar
+  $(".appbar-burger").click(function () {
+    $(".appbar-list").toggleClass("appbar-list-active");
+    $(".appbar-item").each(function (index) {
+      if (this.style.animation) {
+        this.style.animation = '';
+      } else {
+        this.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    })
+    $(".appbar-burger").toggleClass("appbar-toggle");
   });
 
-  appbarBurger.classList.toggle('toggle');
+  // BottomAppBar
+  $(".bottomappbar-bars").click(function () {
+    $(".bottomappbar-list").addClass("bottomappbar-list-active");
+    $(document).scroll(function () {
+      $(".bottomappbar-list").removeClass("bottomappbar-list-active");
+    });
+  });
 });
