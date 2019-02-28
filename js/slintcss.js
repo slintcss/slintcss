f7d8199babc4630628146e10fda02ffdf8cdb950
Cavdy <cavdyofficials@gmail.com>
2019-02-28 23:46:42 +0100

    carousel system created

--- js/slintcss.js
+++ js/slintcss.js
@@ -19,4 +19,34 @@ $(document).ready(function () {
       $(".bottomappbar-list").removeClass("bottomappbar-list-active");
     });
   });
+
+  // Carousel
+  let slideIndex = 1;
+
+  function showImage(n) {
+    var slide = $('.carousel-slides');
+    if (n > slide.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slide.length;
+    }
+
+    for (let i = 0; i < slide.length; i++) {
+      slide[i].style.display = "none";
+    }
+    if (slide.length !== 0) {
+      slide[slideIndex - 1].style.display = "block";
+    }
+  }
+
+  $('.carousel-next').click(function () {
+    showImage(slideIndex += 1);
+  });
+
+  $('.carousel-prev').click(function () {
+    showImage(slideIndex -= 1);
+  })
+
+  showImage(slideIndex);
 });
