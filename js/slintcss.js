721bf7fa61c906740a6e110653887310e6991647
Cavdy <cavdyofficials@gmail.com>
2019-03-01 17:15:55 +0100

    fixed all bugs on carousel

--- js/slintcss.js
+++ js/slintcss.js
@@ -4,11 +4,12 @@ $(document).ready(function () {
     $(".appbar-list").toggleClass("appbar-list-active");
     $(".appbar-item").each(function (index) {
       if (this.style.animation) {
-        this.style.animation = '';
+        this.style.animation = "";
       } else {
-        this.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        this.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
+          0.5}s`;
       }
-    })
+    });
     $(".appbar-burger").toggleClass("appbar-toggle");
   });
 
@@ -21,32 +22,59 @@ $(document).ready(function () {
   });
 
   // Carousel
-  let slideIndex = 1;
+  const carouselFunction = () => {
+    let slideIndex = 1;
 
-  function showImage(n) {
-    var slide = $('.carousel-slides');
-    if (n > slide.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = slide.length;
-    }
+    const carousel = document.querySelector('.carousel');
+    // create prev element
+    const aPrev = document.createElement('a');
+    // add class name
+    aPrev.className = `carousel-prev`;
+    // insert i tag
+    aPrev.innerHTML = `<i class="fa fa-angle-left"></i>`;
+    // append
+    carousel.appendChild(aPrev);
 
-    for (let i = 0; i < slide.length; i++) {
-      slide[i].style.display = "none";
-    }
-    if (slide.length !== 0) {
-      slide[slideIndex - 1].style.display = "block";
-    }
+    // create next element
+    const aNext = document.createElement('a');
+    // add class name
+    aNext.className = `carousel-next`;
+    // insert i tag
+    aNext.innerHTML = `<i class="fa fa-angle-right"></i>`;
+    // append
+    carousel.appendChild(aNext);
+
+    // display Image function
+    const showImage = n => {
+      const slide = $(".carousel-slides");
+      if (n > slide.length) {
+        slideIndex = 1;
+      }
+      if (n < 1) {
+        slideIndex = slide.length;
+      }
+
+      for (let i = 0; i < slide.length; i++) {
+        slide[i].style.display = "none";
+      }
+      if (slide.length !== 0) {
+        slide[slideIndex - 1].style.display = "block";
+      }
+    };
+
+    $(".carousel-next").click(() => {
+      showImage((slideIndex += 1));
+    });
+
+    $(".carousel-prev").click(() => {
+      showImage((slideIndex -= 1));
+    });
+
+    setInterval(() => {
+      showImage((slideIndex += 1));
+    }, 5000);
+
+    showImage(slideIndex);
   }
-
-  $('.carousel-next').click(function () {
-    showImage(slideIndex += 1);
-  });
-
-  $('.carousel-prev').click(function () {
-    showImage(slideIndex -= 1);
-  })
-
-  showImage(slideIndex);
+  carouselFunction();
 });
